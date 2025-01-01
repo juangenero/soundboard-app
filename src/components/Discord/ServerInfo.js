2f1531b788b12d7e1bf8cf5a0601aba809a8c74a
Juan <juangenesp@gmail.com>
2025-01-01 14:58:50 +0100

    aviso mantenimiento

--- src/components/Discord/ServerInfo.js
+++ src/components/Discord/ServerInfo.js
@@ -4,6 +4,9 @@ const ServerInfo = () => {
   return (
     <div>
       <h1>Soundboard App prealfa Early Access On Development alfa beta gamma </h1>
+      <h2>
+        A ese botón no era al que había que darle, el server se ha caido, volvemos en 10 minutos
+      </h2>
       {/* <h2>Server info</h2>
       <p>Servidor: Alcalá City</p>
       <p>Canal: El club de la garraspera</p> */}
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ServerInfo from '../components/Discord/ServerInfo';
-import AddSound from '../components/Sounds/AddSound';
-import ShowSounds from '../components/Sounds/ShowSounds';
 
 function Dashboard(props) {
   return (
     <>
       <ServerInfo />
-      <AddSound />
-      <ShowSounds />
+      {/* <AddSound /> */}
+      {/* <ShowSounds /> */}
     </>
   );
 }
