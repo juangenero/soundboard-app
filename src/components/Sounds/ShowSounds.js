741d5b30f6564c0e929af0a2422f4e3b961b5a83
Juan <juangenesp@gmail.com>
2025-01-02 22:38:36 +0100

    cambiar tabla por tarjetas

--- src/components/Sounds/ShowSounds.js
+++ src/components/Sounds/ShowSounds.js
@@ -1,12 +1,4 @@
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from '@mui/material';
+import { Card, CardActions, CardContent, Grid2, Typography } from '@mui/material';
 import { React, useEffect, useState } from 'react';
 import { getAudios } from '../../services/api.service.js';
 import PlaySound from './PlaySound.js';
@@ -31,28 +23,22 @@ const ShowSounds = () => {
     <>
       {audios.length === 0 && <h2>No hay sonidos para mostrar</h2>}
       {audios.length > 0 && (
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Nombre</TableCell>
-                <TableCell>Audio</TableCell>
-                <TableCell>Acciones</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {audios.map((audio) => (
-                <TableRow key={audio.id}>
-                  <TableCell>{audio.nombre}</TableCell>
-                  <TableCell>{audio.sonido}</TableCell>
-                  <TableCell>
-                    <PlaySound id={audio.id} />
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <Grid2 container spacing={2}>
+          {audios.map((audio) => (
+            <Grid2 xs={12} sm={6} md={4} lg={3} key={audio.id}>
+              <Card sx={{ border: '2px solid lightgray' }}>
+                <CardContent>
+                  <Typography variant="h5" component="div" align="center">
+                    {audio.nombre}
+                  </Typography>
+                </CardContent>
+                <CardActions style={{ justifyContent: 'center' }}>
+                  <PlaySound id={audio.id} />
+                </CardActions>
+              </Card>
+            </Grid2>
+          ))}
+        </Grid2>
       )}
     </>
   );
